761e3509f2e8704e96377596c368652425bfb909
Feodor Fitsner <feodor@appveyor.com>
2018-07-08 15:22:46 -0700

    Testing Add-AppveyorTest

--- test.ps1
+++ test.ps1
@@ -1,1 +1,4 @@
+Add-AppveyorTest -Name "Windows PowerShell" -Framework NUnit -Filename pwsh.exe -Outcome Passed -Duration 1000
+Add-AppveyorTest -Name "PowerShell Core" -Framework NUnit -Filename pwsh.exe -Outcome Passed -Duration 1000
+
 Push-AppVeyorArtifact README.md
