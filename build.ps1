bc4586ddf621ce912d0d2a5512e775b956e52451
Alena Hrynkevich <alena@appveyor.com>
2019-08-16 16:56:18 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 Push-AppVeyorArtifact test.ps1
 pwsh.exe .\test.ps1
-cmd c/ pwsh.exe -Command Add-AppveyorMessage 'Hello, world!'
+#cmd c/ pwsh.exe -Command Add-AppveyorMessage 'Hello, world!'
+Add-AppveyorMessage 'Hello, world!'
